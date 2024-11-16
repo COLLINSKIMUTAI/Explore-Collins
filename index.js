3f85d65fe032628fe564620b4781cc84fb8bde67
Collins Kimutai <ckimutai584@gmail.com>
2024-11-16 15:22:09 +0300

    latest update

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Function to generate a dynamic welcoming message based on the time of day
 function generateWelcomeMessage() {
-  const userName = document.getElementById("userName").value;
-  const welcomeMessageElement = document.getElementById("welcomeMessage");
+  const userName = document.getElementById('userName').value;
+  const welcomeMessageElement = document.getElementById('welcomeMessage');
   const hours = new Date().getHours();
 
   let greeting;
 
   if (hours >= 5 && hours < 12) {
-    greeting = "Good Morning";
+      greeting = 'Good Morning ☀️';
   } else if (hours >= 12 && hours < 18) {
-    greeting = "Good Afternoon";
+      greeting = 'Good Afternoon 🌞';
   } else {
-    greeting = "Good Evening";
+      greeting = 'Good Evening 🌙';
   }
 
   if (userName) {
-    welcomeMessageElement.textContent = `${greeting}, ${userName}! Welcome to our website.`;
+      welcomeMessageElement.textContent = `${greeting}, ${userName}! 🎉 Welcome to my website! 💻`;
   } else {
-    welcomeMessageElement.textContent = `${greeting}! Welcome to our website. Please enter your name.`;
+      welcomeMessageElement.textContent = `${greeting}! 🌟 Please enter your name to get a personalized greeting. 📝`;
   }
 }
